5b6816fa26566506889715f2029c9d484721f4c8
tanmoy12deyashi <tanmoy12deyashi@gmail.com>
2021-06-08 20:29:48 +0530

    'portupdated'

--- main.js
+++ main.js
@@ -2,10 +2,12 @@ const express = require('express');
 //const app = express.Router();
 const app = express();
 
+const port = process.env.PORT || 3000
+
 app.get('/', function(req, res, next) {
     res.send({
         name: 'tanmoy'
     });
 });
 
-app.listen(3000);
+app.listen(port);
