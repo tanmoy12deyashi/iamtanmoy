750ef20b73c629a1b61e9da0a9489b34d58a599f
tanmoy12deyashi <tanmoy12deyashi@gmail.com>
2021-06-08 20:40:17 +0530

    routes added

--- main.js
+++ main.js
@@ -2,11 +2,24 @@ const express = require('express');
 //const app = express.Router();
 const app = express();
 
+// set port
 const port = process.env.PORT || 3000
 
 app.get('/', function(req, res, next) {
     res.send({
         name: 'tanmoy'
+    });
+});
+
+app.get('/about', function(req, res, next) {
+    res.send({
+        page: 'about'
+    });
+});
+
+app.get('/contact', function(req, res, next) {
+    res.send({
+        page: 'contact'
     });
 });
 
