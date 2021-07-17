34883d1b2c00e1246141c3901bd40654b08bf065
tanmoy12deyashi <tanmoy12deyashi@gmail.com>
2021-07-17 17:17:07 +0530

    3rd upload

--- app/controller/blog.js
+++ app/controller/blog.js
@@ -1,8 +1,40 @@
+// exports file module
 const fs = require('fs');
 
-module.exports = {
+// exports blog module
+const $blog = module.exports = {
     index: function (req, res) {
+        // get all blogs
         let blogs = fs.readFileSync(`${SYS_PATH}/app/blog_content/blog.json`, 'utf8');
+
+        // get result blog
         res.send(blogs);
+    },
+    blog: function (req, res) {
+        // get all blogs
+        let blogs = require(`${SYS_PATH}/app/blog_content/blog.json`);
+
+        // get result blog
+        blogs = $blog.search('id', req.params.blog_id, blogs);
+
+        // if blog not exists
+        if(typeof blogs == 'undefined' || !fs.existsSync(`${SYS_PATH}/app/blog_content/${req.params.blog_id}.html`))
+            return res.send({sts: 'Blog not found'});
+
+        // get blog content
+        let blogContent = fs.readFileSync(`${SYS_PATH}/app/blog_content/${req.params.blog_id}.html`);
+
+        // render b.log
+        res.render('blog', {blogs: blogs, blogContent: blogContent});
+    },
+    search: function(key, val, arr) {
+        // loop array
+        for (var i=0; i < arr.length; i++) {
+            // check key value
+            if (arr[i][key] == val) {
+                // return result
+                return arr[i];
+            }
+        }
     }
 }
--- app/public/js/index.js
+++ app/public/js/index.js
@@ -2,6 +2,7 @@ window.onload = function() {
   getAllPost();
 }
 
+// get all post
 function getAllPost() {
   // set options
   let options = {
@@ -17,8 +18,50 @@ function getAllPost() {
 
   // get all post
   allPost.then(res =>res.json())
-    .then(post => {
-      console.log(post);
+    .then(posts => {
+      console.log(posts);
+      console.log(dateTimeFormat("02-02-2021", "MMMM dd, yyyy"));
+      // if post found
+      if(posts.length) {
+        // set degault html
+        let html = '', counter = 1;
+
+        // loop posts
+        posts.forEach(post => {
+          // update html
+          html += `<div class="post-preview">
+                    <a href="blog/${post.id}">
+                        <h2 class="post-title">${post.title}</h2>
+                        <h3 class="post-subtitle">${post.sub_title}</h3>
+                    </a>
+                    <p class="post-meta">
+                        Posted by
+                        <a href="/about">${post.created_by}</a>
+                        on ${dateTimeFormat(post.create_date, "MMMM dd, yyyy")}
+                    </p>
+                  </div>
+                  <hr class="my-4" />`;
+          // update counter
+          counter++;
+
+          // if last post
+          if(counter == posts.length) {
+            setTimeout(function() {
+              // hide loader
+              document.querySelectorAll('.loader')[0].classList.add('d-none');
+
+              // Get the reference node
+              var postNode = document.querySelector('#all-post');
+
+              // Insert new html to the node
+              postNode.innerHTML = html;
+            }, 500);
+            
+          }
+        });
+      } else {
+        console.log('no post found')
+      }
     }
   )
 }
